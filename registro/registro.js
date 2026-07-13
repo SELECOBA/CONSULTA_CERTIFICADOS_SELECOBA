0cb961fe9679e706b3bb9bbe43896023ed2391d2
SELECOBA <info.selecoba@gmail.com>
2026-07-13 15:02:34 -0500

    Update registro.js

--- registro/registro.js
+++ registro/registro.js
@@ -434,3 +434,55 @@ observaciones.value="";
 
 
 }
+
+//=========================================
+// FECHA AUTOMATICA
+//=========================================
+
+
+window.onload=function(){
+
+
+const fecha =
+document.getElementById("fecha");
+
+
+
+if(fecha){
+
+
+const hoy =
+new Date();
+
+
+
+const dia =
+String(
+hoy.getDate()
+)
+.padStart(2,"0");
+
+
+
+const mes =
+String(
+hoy.getMonth()+1
+)
+.padStart(2,"0");
+
+
+
+const anio =
+hoy.getFullYear();
+
+
+
+fecha.value =
+dia+"/"+mes+"/"+anio;
+
+
+
+}
+
+
+};
